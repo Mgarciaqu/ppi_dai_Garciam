0753ee4b62cfa9928c4af3125f2c5c2d7c101242
Michael Garcia <124626393+Mgarciaqu@users.noreply.github.com>
2024-09-02 04:06:07 -0500

    Update modelos.py

--- celuya_app/modelos.py
+++ celuya_app/modelos.py
@@ -1,1 +1,17 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class UsuarioPersonalizado(AbstractUser):
+    pass
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    marca = models.CharField(max_length=50)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    descripcion = models.TextField()
+    imagen = models.ImageField(upload_to='productos/')
+    stock = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.nombre
 
