5f9560784cd0377b71b3b0d8492def6186617000
Michael Garcia <124626393+Mgarciaqu@users.noreply.github.com>
2024-09-02 10:16:13 -0500

    Delete streamlit_app.py

--- streamlit_app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import os
-import django
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'celuya.configuracion')
-django.setup()
-
-st.title("Celuya!")
-st.write("Bienvenido a la tienda de celulares en línea.")
