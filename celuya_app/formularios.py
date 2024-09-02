6ed35bc2fe5d9eeb5d9ae67ea5879d989c4a8d20
Michael Garcia <124626393+Mgarciaqu@users.noreply.github.com>
2024-09-02 04:06:24 -0500

    Update formularios.py

--- celuya_app/formularios.py
+++ celuya_app/formularios.py
@@ -1,1 +1,9 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .modelos import UsuarioPersonalizado
+
+class FormularioCreacionUsuario(UserCreationForm):
+    class Meta:
+        model = UsuarioPersonalizado
+        fields = ('username', 'email', 'password1', 'password2')
 
