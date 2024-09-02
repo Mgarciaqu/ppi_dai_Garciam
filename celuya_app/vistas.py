4a7d1a69d07dac5f8c531e979166dbe7e5dd86bd
Michael Garcia <124626393+Mgarciaqu@users.noreply.github.com>
2024-09-02 04:06:45 -0500

    Update vistas.py

--- celuya_app/vistas.py
+++ celuya_app/vistas.py
@@ -1,1 +1,15 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .formularios import FormularioCreacionUsuario
+
+def registro(request):
+    if request.method == 'POST':
+        formulario = FormularioCreacionUsuario(request.POST)
+        if formulario.is_valid():
+            usuario = formulario.save()
+            login(request, usuario)
+            return redirect('inicio')
+    else:
+        formulario = FormularioCreacionUsuario()
+    return render(request, 'registro.html', {'formulario': formulario})
 
