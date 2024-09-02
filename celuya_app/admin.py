8173e41633fc4bc811e8c37c25d491c2d98353e7
Michael Garcia <124626393+Mgarciaqu@users.noreply.github.com>
2024-09-02 10:14:31 -0500

    Delete celuya_app directory

--- celuya_app/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .modelos import UsuarioPersonalizado, Producto
-
-admin.site.register(UsuarioPersonalizado)
-admin.site.register(Producto)
-
--- celuya_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import vistas
-
-urlpatterns = [
-    path('registro/', vistas.registro, name='registro'),
-]
-
